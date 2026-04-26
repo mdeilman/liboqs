11317bb741731ae2d361d4637e82db7396363acb
Mario Deilmann <mario.deilmann@googlemail.com>
2026-04-26 12:12:48 +0200

    chore: remove auxiliary test and patch files

--- mad_test/build.sh
+++
@@ -1,6 +0,0 @@
-gcc test_lms_nist.c \
-    -I/home/mad/liboqs/src/sig_stfl/lms/external \
-    -I/home/mad/liboqs/build/include \
-    -L/home/mad/liboqs/build/lib \
-    -Wl,-rpath,/home/mad/liboqs/build/lib \
-    -loqs -lssl -lcrypto -o test_lms_nist && ./test_lms_nist
--- mad_test/test_lms_nist.sh
+++
@@ -1,8 +0,0 @@
-for variant in \
-	    LMS_SHA256_N24_H5_W1 LMS_SHA256_N24_H5_W8 \
-	        LMS_SHAKE_N32_H5_W1 LMS_SHAKE_N32_H5_W8 \
-		    LMS_SHAKE_N24_H5_W1 LMS_SHAKE_N24_H5_W8; do
-    result=$(~/liboqs/build/tests/test_sig_stfl $variant 2>&1 | \
-	                 grep -E "Passed|Failed|not enabled" | tail -1)
-        echo "$variant: $result"
-done
